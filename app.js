7883efc45cf89a64e38e63da5704161b65d069f4
harshaljoshi1 <mamtabhardwaj12@gmail.com>
2018-04-13 19:08:58 +0530

    Switch to hello MEANSTACK

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello MEAN');
+  res.send('hello MEANSTACK');
 });
  
 app.listen(process.env.PORT || 5000);
