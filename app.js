9eec82969110fda11c42f82d5a123f70dc75d070
harshaljoshi1 <mamtabhardwaj12@gmail.com>
2018-04-13 18:57:50 +0530

    Switch to hello MEAN

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello Node');
+  res.send('hello MEAN');
 });
  
 app.listen(process.env.PORT || 5000);
