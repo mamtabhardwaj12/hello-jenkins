6b00ce57dd12983c147fe5d30503abf13cf48536
harshaljoshi1 <mamtabhardwaj12@gmail.com>
2018-04-14 10:06:46 +0530

    Switch to hello g

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello b');
+  res.send('hello g');
 });
  
 app.listen(process.env.PORT || 5000);
