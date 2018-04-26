cd4409f890e04e5afa0d9be3d3cec11be96af560
Cateina Technologies Pvt Ltd <cateina@ip-172-31-44-173.us-west-2.compute.internal>
2018-04-26 11:01:54 +0530

    Switch to hello cateina

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello world');
+  res.send('hello cateina');
 });
  
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 5002);
  
 module.exports = app;
 
